2dc216adc130f525d37bb551934897d2a212dee0
mariocan34 <mariocg61@educastur.es>
2025-10-30 11:58:46 +0100

    Cambio de atributo tamaño a size

--- model/Bebida.php
+++ model/Bebida.php
@@ -2,12 +2,12 @@
 // Clase Bebida que hereda de Artículo
 
 class Bebida extends Articulo {
-    public string $tamaño;
+    public string $size;
     public string $temperatura;
 
-    public function __construct(string $nombre, float $precio, bool $disponibilidad, string $categoria, string $tamaño, string $temperatura) {
+    public function __construct(string $nombre, float $precio, bool $disponibilidad, string $categoria, string $size, string $temperatura) {
         parent::__construct($nombre, $precio, $disponibilidad, $categoria);
-        $this->tamaño = $tamaño;
+        $this->size = $size;
         $this->temperatura = $temperatura;
     }
 }
