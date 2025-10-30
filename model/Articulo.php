6a16b719ab1cf72557fc556abfdd2cae59af2b4c
mariocan34 <mariocg61@educastur.es>
2025-10-30 11:50:18 +0100

    Creacion de la clase articulo

--- model/Articulo.php
+++ model/Articulo.php
@@ -1,4 +1,17 @@
 <?php
 // Clase Artículo
 
+class Articulo {
+    public string $nombre;
+    public float $precio;
+    public bool $disponibilidad;
+    public string $categoria;
+
+    public function __construct(string $nombre, float $precio, bool $disponibilidad, string $categoria) {
+        $this->nombre = $nombre;
+        $this->precio = $precio;
+        $this->disponibilidad = $disponibilidad;
+        $this->categoria = $categoria;
+    }
+}
 ?>
